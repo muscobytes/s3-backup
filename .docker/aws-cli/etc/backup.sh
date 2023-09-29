e5a063b95423ba4389e3f252b22007d5a6f9474a
postfriday <postfriday@users.noreply.github.com>
2023-09-30 00:48:25 +0400

    simplify backup.sh

--- .docker/aws-cli/etc/backup.sh
+++ .docker/aws-cli/etc/backup.sh
@@ -5,7 +5,7 @@ BACKUP_DIR=/backup
 BACKUP_PATH=${BACKUP_DIR}/${PROJECT_NAME}_$(date +%Y%m%d_%H%I%S).tar.gz
 
 mkdir -p ${BACKUP_DIR} \
-    && tar -czf ${BACKUP_PATH} ${MEDIA_DIR} \
+    && tar -czf ${BACKUP_PATH} /media \
     && aws \
         --endpoint-url=https://storage.yandexcloud.net \
         --region=ru-central1 \
