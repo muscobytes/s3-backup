d7973f86feb44a41fc885f2235a5ee9fdc1eeb46
postfriday <postfriday@users.noreply.github.com>
2023-09-30 03:27:55 +0400

    fix

--- .docker/aws-cli/etc/backup.sh
+++ .docker/aws-cli/etc/backup.sh
@@ -6,10 +6,10 @@ BACKUP_PATH=/${PROJECT_NAME}_$(date +%Y%m%d_%H%I%S).tar.gz
 
 mkdir -p ${BACKUP_DIR}/sql \
     && mysqldump --host ${MYSQL_HOST} \
-        --port ${MYSQL_PORT} \
-        --user ${MYSQL_USER} \
-        --password ${MYSQL_PASSWORD} \
-        --databases ${MYSQL_DATABASE} \
+        --port=${MYSQL_PORT} \
+        --user=${MYSQL_USER} \
+        --password=${MYSQL_PASSWORD} \
+        --databases=${MYSQL_DATABASE} \
         > ${BACKUP_DIR}/sql/${MYSQL_DATABASE}.sql \
     && tar -czf --totals --checkpoint=5000 ${BACKUP_PATH} ${BACKUP_DIR} \
     && aws \
