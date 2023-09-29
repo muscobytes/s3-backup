08db30ab96713e9213a0b3d8c8a61a8a369cf669
postfriday <postfriday@users.noreply.github.com>
2023-09-30 02:29:45 +0400

    fix

--- .docker/aws-cli/etc/backup.sh
+++ .docker/aws-cli/etc/backup.sh
@@ -5,8 +5,10 @@ BACKUP_DIR=/backup
 BACKUP_PATH=${BACKUP_DIR}/${PROJECT_NAME}_$(date +%Y%m%d_%H%I%S).tar.gz
 
 mkdir -p ${BACKUP_DIR} \
-    && tar -czf --totals --checkpoint=5000 ${BACKUP_PATH} /media \
+    && tar cf - /media | pv -s $(du -sb /media | awk '{print $1}') | gzip > ${BACKUP_PATH} \
     && aws \
         --endpoint-url=https://storage.yandexcloud.net \
         --region=ru-central1 \
         s3 cp ${BACKUP_PATH} s3://renaissance-backups/${PROJECT_NAME}/
+
+# && tar -czf --totals --checkpoint=5000 ${BACKUP_PATH} /media \
