4c545422a2f1a24f0ecbf40182f1cede583b63ee
postfriday <postfriday@users.noreply.github.com>
2023-09-30 02:57:22 +0400

    fix

--- .docker/aws-cli/etc/backup.sh
+++ .docker/aws-cli/etc/backup.sh
@@ -2,13 +2,12 @@
 set -x
 
 BACKUP_DIR=/backup
-BACKUP_PATH=${BACKUP_DIR}/${PROJECT_NAME}_$(date +%Y%m%d_%H%I%S).tar.gz
+BACKUP_PATH=/${PROJECT_NAME}_$(date +%Y%m%d_%H%I%S).tar.gz
 
 mkdir -p ${BACKUP_DIR} \
-    && tar cf - /media | pv -s $(du -sb /media | awk '{print $1}') | gzip > ${BACKUP_PATH} \
+    && mysqldump -u ${MYSQL_USER} -p ${MYSQL_PASSWORD} —-databases ${MYSQL_DATABASE}> ${BACKUP_DIR}/sql/${MYSQL_DATABASE}.sql \
+    && tar -czf --totals --checkpoint=5000 ${BACKUP_PATH} ${BACKUP_DIR}/media \
     && aws \
         --endpoint-url=https://storage.yandexcloud.net \
         --region=ru-central1 \
         s3 cp ${BACKUP_PATH} s3://renaissance-backups/${PROJECT_NAME}/
-
-# && tar -czf --totals --checkpoint=5000 ${BACKUP_PATH} /media \
