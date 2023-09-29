4e2219c6f1b66a105e1a2dc6f7de8bb67c282946
postfriday <postfriday@users.noreply.github.com>
2023-09-30 03:22:14 +0400

    fix

--- .docker/aws-cli/etc/backup.sh
+++ .docker/aws-cli/etc/backup.sh
@@ -4,14 +4,14 @@ set -x
 BACKUP_DIR=/backup
 BACKUP_PATH=/${PROJECT_NAME}_$(date +%Y%m%d_%H%I%S).tar.gz
 
-mkdir -p ${BACKUP_DIR} \
+mkdir -p ${BACKUP_DIR}/sql \
     && mysqldump --host ${MYSQL_HOST} \
         --port ${MYSQL_PORT} \
         --user ${MYSQL_USER} \
         --password ${MYSQL_PASSWORD} \
         --databases ${MYSQL_DATABASE} \
         > ${BACKUP_DIR}/sql/${MYSQL_DATABASE}.sql \
-    && tar -czf --totals --checkpoint=5000 ${BACKUP_PATH} ${BACKUP_DIR}/media \
+    && tar -czf --totals --checkpoint=5000 ${BACKUP_PATH} ${BACKUP_DIR} \
     && aws \
         --endpoint-url=https://storage.yandexcloud.net \
         --region=ru-central1 \
