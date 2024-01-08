dc0e680d01e1356e20970fc923cd431416ca0ad1
Stanislav Surskiy <ssurskiy@gmail.com>
2024-01-08 14:33:08 +0400

    fix: backup.sh

--- .docker/aws-cli/etc/backup.sh
+++ .docker/aws-cli/etc/backup.sh
@@ -4,11 +4,12 @@ set -x
 TARGET_DIR=/backup
 BACKUP_PATH=/opt/${PROJECT_NAME}_$(date +%Y%m%d_%H%I%S).tar.gz
 
-MYSQL_DUMP_DIR=${TARGET_DIR}/mysql
 MYSQL_PORT=${MYSQL_PORT:-3306}
+MYSQL_DUMP_DIR=${TARGET_DIR}/mysql
 
 POSTGRES_USER=${POSTGRES_USER:-postgres}
 POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_DUMP_DIR=${TARGET_DIR}/postgres
 
 TAR_CHECKPOINT=${TAR_CHECKPOINT:-5000}
 
@@ -17,32 +18,31 @@ S3_PATH=${S3_PATH:-renaissance_backup}
 S3_REGION=${S3_REGION:-ru-central1}
 
 [ -z "${PROJECT_NAME}" ] \
-    && echo "PROJECT_NAME is not set" && exit 1
+    && echo "PROJECT_NAME is not set" && exit 100
 
 [ -z "${AWS_SECRET_ACCESS_KEY}" ] \
-    && echo "AWS_SECRET_ACCESS_KEY is not set" && exit 1
+    && echo "AWS_SECRET_ACCESS_KEY is not set" && exit 101
 
 [ -z "${AWS_ACCESS_KEY_ID}" ] \
-    && echo "AWS_ACCESS_KEY_ID is not set" && exit 1
+    && echo "AWS_ACCESS_KEY_ID is not set" && exit 102
 
 ################################################################################
 # MySQL backup
 ################################################################################
 if [ -n "${MYSQL_HOST}" ] \
-    && [ -n "${MYSQL_PORT}" ] \
     && [ -n "${MYSQL_USER}" ] \
     && [ -n "${MYSQL_PASSWORD}" ] \
     && [ -n "${MYSQL_DATABASE}" ]
 then
-    echo "MySQL backup enabled"
-    mkdir -p ${TARGET_DIR}/mysql && \
-        mysqldump --host ${MYSQL_HOST} \
-            --port=${MYSQL_PORT} \
-            --user=${MYSQL_USER} \
-            --password=${MYSQL_PASSWORD} \
-            --databases ${MYSQL_DATABASE} \
-            --no-tablespaces \
-            > ${TARGET_DIR}/mysql/${MYSQL_DATABASE}.sql
+    echo "MySQL backup enabled" \
+    && mkdir --parent ${MYSQL_DUMP_DIR} \
+    && mysqldump --host ${MYSQL_HOST} \
+        --port=${MYSQL_PORT} \
+        --user=${MYSQL_USER} \
+        --password=${MYSQL_PASSWORD} \
+        --databases ${MYSQL_DATABASE} \
+        --no-tablespaces \
+        > ${MYSQL_DUMP_DIR}/${MYSQL_DATABASE}.sql
 else
     echo "MySQL backup disabled"
 fi
@@ -51,20 +51,18 @@ fi
 # PostgreSQL backup
 ################################################################################
 if [ -n "${POSTGRES_HOST}" ] \
-    && [ -n "${POSTGRES_PORT}" ] \
-    && [ -n "${POSTGRES_USER}" ] \
     && [ -n "${POSTGRES_PASSWORD}" ] \
     && [ -n "${POSTGRES_DATABASE}" ]
 then
-    echo "PostreSQL backup enabled"
-    mkdir -p ${TARGET_DIR}/postgres \
+    echo "PostreSQL backup enabled" \
+    && mkdir --parent ${POSTGRES_DUMP_DIR} \
     && PGPASSWORD="${POSTGRES_PASSWORD}" pg_dump \
         --host=${POSTGRES_HOST} \
         --port=${POSTGRES_PORT} \
         --username=${POSTGRES_USER} \
         --format=plain \
         --verbose \
-        --file=${TARGET_DIR}/postgres/${POSTGRES_DATABASE}.dump \
+        --file=${POSTGRES_DUMP_DIR}/${POSTGRES_DATABASE}.dump \
         ${POSTGRES_DATABASE}
 else
     echo "PostgreSQL backup disabled"
